a36ec1697f26c8be7e436ee3c613cec88e4484c5
K-RKidd <kr248196@muhlenberg.edu>
2014-04-11 10:40:08 -0400

    Initial Commit

---
+++ Quiz8/Quiz8AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz8AppDelegate.h
+//  Quiz8
+//
+//  Created by Krystle on 4/11/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz8AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz8/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz8
+//
+//  Created by Krystle on 4/11/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Quiz8AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz8AppDelegate class]));
+    }
+}
